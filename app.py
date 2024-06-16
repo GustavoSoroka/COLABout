3260daad4a9863e313445e008430a83bea0463b5
GustavoSoroka <68706250+GustavoSoroka@users.noreply.github.com>
2024-06-16 17:12:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 
-#import google.auth
+import google.auth
 
 from google.oauth2 import service_account
 
@@ -35,10 +35,15 @@ st.markdown("""
 
 
 
-
-credenciais = service_account.Credentials.from_service_account_info(
-    st.secrets["gsc"]
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive']
+my_credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gsc"],
+    scopes=SCOPES
+   
 )
+
+
+credenciais = pygsheets.authorize(custom_credentials=my_credentials)
 
 
 my_file = st.secrets['path']['sheets']
@@ -60,18 +65,20 @@ st.title('Simulation Dashboard')
 
 
 
-c1, c2 = st.columns([1,1])
+c1, c2, c3 = st.columns([1,1,1])
 
 
 with c1:
+    st.subheader('Output')
     st.data_editor(df)
     
 with c2:
+    st.subheader('Plot')
     fig = px.line(df,x = 'Vocab Train', y = 'ACC Train', markers = True)
     fig.update_layout(margin=dict(t=0, b=0), height=300,yaxis_range=[0,1])
     st.plotly_chart(fig, use_container_width=True, theme="streamlit", key=None, on_select="ignore", selection_mode=('points', 'box', 'lasso'), config={"displayModeBar": False})
     
 
-sleep(5)
+sleep(10)
 
 st.rerun()
